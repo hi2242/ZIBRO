e490aba4c2e6559e62bc5812789a76f63f79fa2c
hi2242 <doodukly1999@gmail.com>
2025-04-09 19:17:34 +0900

    [fix] 구글 맵 API키 .env 로컬처리

--- clone/ZIBRO_FRONT_END/ios/Runner/AppDelegate.swift
+++ clone/ZIBRO_FRONT_END/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyAKUIRQLZLg39Z7SDr1WA6Jc0tDYYDfzSk")
+    GMSServices.provideAPIKey(${GoogleMaps})
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
